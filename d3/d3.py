58a99e5d0922a70a6bdeffa26cb2108a9a77c63e
Sebastian Pauka <sebastian.pauka@sydney.edu.au>
2020-12-03 17:01:40 +1100

    d3 improvements

--- d3/d3.py
+++ d3/d3.py
@@ -1,17 +1,13 @@
 import numpy as np
 
 with open("input.txt") as f:
-    lines = [l.strip() for l in f]
-    g = np.ndarray((len(lines[0]), len(lines)), dtype="object")
-    for i, line in enumerate(lines):
-        g[:,i] = list(line)
+    g = np.array(list(list(l.strip()) for l in f), dtype="object").T
 
 def traverse(g, slope):
     pos = np.array((0, 0))
     count = 0
     while pos[1] < g.shape[1]:
-        if g[pos[0]%g.shape[0], pos[1]] == '#':
-            count += 1
+        count += (g[pos[0]%g.shape[0], pos[1]] == '#')
         pos += slope
     return count
 

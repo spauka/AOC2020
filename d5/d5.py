90ef9e8791ed4bd000f73ce687104a75df5c5232
Sebastian Pauka <spauka@gmail.com>
2020-12-05 20:10:17 +1100

    More simplifications

--- d5/d5.py
+++ d5/d5.py
@@ -1,8 +1,7 @@
+from operator import itemgetter
 def partition(start, stop, part):
-    mid = start + (stop - start)//2
-    if part:
-        return mid+1, stop
-    return start, mid
+    mid = start + ((stop - start)>>1)
+    return (mid+1, stop) if part else (start, mid)
 
 seats = []
 with open("input") as f:
@@ -15,14 +14,13 @@ with open("input") as f:
             for c in line[7:]:
                 pc = partition(*pc, (c=="R"))
             seats.append((pr[0], pc[0], (pr[0] << 3) + pc[0]))
-seats.sort(key=lambda x: x[2])
+seats.sort(key=itemgetter(2))
 
 print(f"Highest ID: {seats[-1][2]}")
 
 missing = 0
 for i in range(seats[-1][2]):
-    seat = seats[i-missing]
-    if i != seat[2]:
+    if i != seats[i-missing][2]:
         missing += 1
         if (i-missing) == -1:
             continue

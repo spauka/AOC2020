0062c7a1f64ed7c4886fc4f957402f5a1cdbdc3a
Sebastian Pauka <sebastian.pauka@sydney.edu.au>
2020-12-15 17:09:27 +1100

    Remove unessecary lines

--- d15/d15.py
+++ d15/d15.py
@@ -4,10 +4,8 @@ inp = [0,5,4,1,10,14,7]
 seen = {n: i for i, n in enumerate(inp[:-1])}
 ind = inp[-1]
 
-for i in range(i-1, 30000000-1):
+for i in range(len(inp)-1, 30000000-1):
     nind = i - seen.get(ind, i)
     seen[ind] = i
     ind = nind
-
-print()
 print(f"Part 2: {ind}")

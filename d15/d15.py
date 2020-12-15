603e10afe5a783a9e72501d7e8f4c04d1ab5f6ff
Sebastian Pauka <sebastian.pauka@sydney.edu.au>
2020-12-15 16:36:55 +1100

    optimize d15

--- d15/d15.py
+++ d15/d15.py
@@ -5,13 +5,10 @@ seen = {n: i for i, n in enumerate(inp[:-1])}
 i = len(inp)
 ind = inp[-1]
 
-for i in range(i, 30000000):
-    nind = (i - 1) - seen.get(ind, i - 1)
-    seen[ind] = i - 1
+for i in range(i-1, 30000000-1):
+    nind = i - seen.get(ind, i)
+    seen[ind] = i
     ind = nind
-
-    if i%10000 == 0:
-        print(f"N: {i:7}", end="\r")
 
 print()
 print(f"Part 2: {ind}")
